c4d8de7cf9607f397cb7adcf074458cbb51f71ff
btctrm <btctrm@gmail.com>
2026-03-09 20:12:27 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -1,31 +1,21 @@
-import { BeiAnGongAn } from '@/components/BeiAnGongAn'
 import { siteConfig } from '@/lib/config'
-import SocialButton from './SocialButton'
 
-const Footer = ({ siteInfo }) => {
+const Footer = () => {
   return (
     <footer className='z-20 border p-2 rounded-lg bg:white dark:border-black dark:bg-hexo-black-gray justify-center text-center w-full text-sm relative'>
-      <SocialButton />
+      {/*
+      访问量 / 访客数统计，当前隐藏。
+      以后如果要恢复，删掉这段注释即可。
 
-      {siteConfig('BEI_AN') && (
-        <>
-          <i className='fas fa-shield-alt' />{' '}
-          <a href={siteConfig('BEI_AN_LINK')} className='mr-2'>
-            {siteConfig('BEI_AN')}
-          </a>
-          <BeiAnGongAn />
-          <br />
-        </>
-      )}
-
-      <span className='hidden busuanzi_container_site_pv'>
+      <span className='busuanzi_container_site_pv'>
         <i className='fas fa-eye' />
         <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
       </span>
-      <span className='pl-2 hidden busuanzi_container_site_uv'>
+      <span className='pl-2 busuanzi_container_site_uv'>
         <i className='fas fa-users' />{' '}
         <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
       </span>
+      */}
 
       <div className='text-xs font-serif'>
         Powered By{' '}
