0824ed44c315ee2f7fb9d7b95ffa5dcbadda41cd
btctrm <btctrm@gmail.com>
2026-03-09 19:59:35 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -1,34 +1,11 @@
 import { BeiAnGongAn } from '@/components/BeiAnGongAn'
 import { siteConfig } from '@/lib/config'
 import SocialButton from './SocialButton'
-/**
- * 站点也叫
- * @param {*} param0
- * @returns
- */
-const Footer = ({ siteInfo }) => {
-  const d = new Date()
-  const currentYear = d.getFullYear()
-  const since = siteConfig('SINCE')
-  const copyrightDate =
-    parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
+const Footer = ({ siteInfo }) => {
   return (
     <footer className='z-20 border p-2 rounded-lg bg:white dark:border-black dark:bg-hexo-black-gray justify-center text-center w-full text-sm relative'>
       <SocialButton />
-
-      <div className='flex justify-center'>
-        <div>
-          <i className='mx-1 animate-pulse fas fa-heart' />{' '}
-          <a
-            href={siteConfig('LINK')}
-            className='underline font-bold text-gray-500 dark:text-gray-300 '>
-            {siteConfig('AUTHOR')}
-          </a>
-          .<br />
-        </div>
-        © {`${copyrightDate}`}
-      </div>
 
       {siteConfig('BEI_AN') && (
         <>
@@ -49,6 +26,7 @@ const Footer = ({ siteInfo }) => {
         <i className='fas fa-users' />{' '}
         <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
       </span>
+
       <div className='text-xs font-serif'>
         Powered By{' '}
         <a
@@ -57,7 +35,7 @@ const Footer = ({ siteInfo }) => {
           NotionNext {siteConfig('VERSION')}
         </a>
       </div>
-      {/* SEO title */}
+
       <h1 className='pt-1 hidden'>{siteConfig('TITLE')}</h1>
     </footer>
   )
